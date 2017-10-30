0c16a3f51973638b0fc18743a3ee56401c3468dd
Marco Solazzi <marco.solazzi@gmail.com>
2017-10-30 21:57:58 +0100

    feat(repowise): converted to new umeboshi-cli template format

---
+++ template/app/__vue/containers/Root/index.vue
@@ -0,0 +1,27 @@
+<template>
+    <div :class="$style.root">
+        Hello \{{ message }}
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'Root',
+
+    data() {
+        return {
+            message: 'World'
+        };
+    }
+};
+
+</script>
+
+<style lang="scss" module>
+@import "mixins";
+
+.root {
+    font-size: font-size(13px);
+}
+</style>
